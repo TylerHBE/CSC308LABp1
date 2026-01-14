66794c8535f1db18136b84437de6917042966c1e
TylerHBE <tylerhickersonbe@gmail.com>
2026-01-13 17:34:05 -0800

    Added additional functionality

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -42,12 +42,25 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
+};
+
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
 const addUser = (user) => {
   users["users_list"].push(user);
   return user;
+};
+
+const deleteUser = (user) => {
+  users["users_list"] = users["users_list"].filter(
+    (u) => u["id"] !== user["id"]
+  );
+  return null;
 };
 
 // endpoints
@@ -59,7 +72,13 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
+  const job = req.query.job;
+  if (name != undefined && job != undefined) {
+    let result = findUserByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);
+  }
+  else if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
     res.send(result);
@@ -82,6 +101,17 @@ app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
+});
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    deleteUser(result);
+    res.end();
+  }
 });
 
 app.listen(port, () => {
